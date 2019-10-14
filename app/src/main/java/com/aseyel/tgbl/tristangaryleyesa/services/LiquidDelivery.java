e7359e900f3c0980cf7d1afdd2fb21c532b4d354
IT-Jp <jpcysalina@ngcruz.com>
2019-10-14 14:09:41 +0800

    upload all messengerial data preparation

--- app/src/main/java/com/aseyel/tgbl/tristangaryleyesa/services/LiquidDelivery.java
+++ app/src/main/java/com/aseyel/tgbl/tristangaryleyesa/services/LiquidDelivery.java
@@ -20,12 +20,15 @@ public class LiquidDelivery {
         try{
             JSONObject final_data_response = new JSONObject();
             JSONArray final_response = new JSONArray();
+            JSONArray all_data = new JSONArray();
 
 
             final_response = UploadDelivery(JobID,"");
+            all_data = UploadAllDelivery(JobID,"");
 
 
             final_data_response.put("data",final_response);
+            final_data_response.put("all_data",all_data);
 
             return final_data_response;
         }catch(Exception e){
@@ -37,6 +40,51 @@ public class LiquidDelivery {
     public static JSONArray UploadDelivery(String JobOrderId,String TrackingNumber){
         JSONArray final_response = new JSONArray();
         Cursor result = DeliveryModel.GetMessengerialForUpload(JobOrderId, TrackingNumber,"Pending");
+        if (result.getCount() == 0) {
+            return final_response;
+        }
+
+        while(result.moveToNext()){
+            JSONObject data = new JSONObject();
+            try {
+
+                data.put("client",result.getString(0));
+                data.put("job_id",result.getString(1));
+                data.put("ngc_job_id","1");
+                data.put("job_title",result.getString(2));
+                data.put("trackingNumber",result.getString(3));
+                data.put("itemType",result.getString(4));
+                data.put("itemTypeDescription",result.getString(5));
+                data.put("status",result.getString(6));
+                data.put("remarksCode",result.getString(7));
+                data.put("remarks",result.getString(8));
+                data.put("comment",result.getString(9));
+                data.put("latitude",result.getString(10));
+                data.put("longitude",result.getString(11));
+                data.put("signature",result.getString(13));
+                data.put("date",result.getString(17));
+                data.put("timeStamp",result.getString(16));
+                data.put("userID",result.getString(18));
+                data.put("transferDataStatus",result.getString(12));
+                data.put("batteryLife",result.getString(17));
+                data.put("username",Liquid.Username);
+                data.put("password",Liquid.Password);
+                data.put("deviceserial", Build.SERIAL);
+                data.put("sysid",Liquid.User);
+                Log.i(TAG, String.valueOf(data));
+                final_response.put(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return final_response;
+    }
+
+    public static JSONArray UploadAllDelivery(String JobOrderId,String TrackingNumber){
+        JSONArray final_response = new JSONArray();
+        Cursor result = DeliveryModel.GetAllMessengerialForUpload(JobOrderId, TrackingNumber);
         if (result.getCount() == 0) {
             return final_response;
         }
